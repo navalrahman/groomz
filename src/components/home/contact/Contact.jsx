b1cb5f5140f61d0ec16e682ddd8149ebe2ebecaa
navalrahmanlevon <navalrahman.levontechno@gmail.com>
2025-04-18 19:26:15 +0530

    picture added

--- src/components/home/contact/Contact.jsx
+++ src/components/home/contact/Contact.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './Contact.css'
 
-// import logo from '../../../images/instagram.jpg'
-import logo from '../../../../public/instagram.jpg'
+import logo from '../../../images/instagram.jpg'
+// import logo from '../../../../public/instagram.jpg'
 
 const handleInstagramClick = () => {
     const instgramUrl = "https://www.instagram.com/groomz_wedding/"; // example: Delhi coordinates
@@ -16,12 +16,14 @@ const Contact = () => {
     };
 
     const handleCallTwo = () => {
-        window.opene(`tel:${+919645773395}`)
+        window.open(`tel:${+919645773395}`)
     }
     return (
         <div className='contact'>
             <div className='contat-container'>
-                <img src={logo} alt="instagram" width={"50px"} height={"50px"} onClick={handleInstagramClick} />
+                <img
+                    src={logo} 
+                    alt="instagram" width={"50px"} height={"50px"} onClick={handleInstagramClick} />
                 <h3>Edavannappara</h3>
                 <h5>Kozhikode Road </h5>
                 <h5>Malappuram</h5>
--- src/components/home/mensapparels/Mensapparels.jsx
+++ src/components/home/mensapparels/Mensapparels.jsx
@@ -11,33 +11,65 @@ import five from '../../../images/apparels/five.jpg'
 import six from '../../../images/apparels/six.jpg'
 import seven from '../../../images/apparels/seven.jpg'
 
+import groomone from '../../../images/groomz/groom_one.jpg'
+import groomtwo from '../../../images/groomz/groom_two.jpg'
+import groomthree from '../../../images/groomz/groom_three.jpg'
+import groomfour from '../../../images/groomz/groom_four.jpg'
+
 import './Mensapparels.css'
 
 const Mensapparels = () => {
 
-    const groomzImages = [
-        // "https://www.instagram.com/groomz_wedding/p/DDqtaOkPPjF/?img_index=1",
-        "https://www.instagram.com/r_a_s_h_i_d__96/p/DDcY-6fywBv/"]
-
     const [currentIndex, setCurrentIndex] = useState(0)
+    const [index, setIndex] = useState(0)
+    const [fadeClass, setFadeClass] = useState('fade-image');
+    const [groomFadeClass, setGroomFadeClass] = useState('fade-image');
 
     const images = [one, two, three, four, five, six, seven]
+    const groomImages = [groomone, groomtwo, groomthree, groomfour]
+    // const handleLeftClick = () => {
+    //     setCurrentIndex((prev) => (prev === 0 ? images.length - 1 : prev - 1))
+    // }
 
-    const handleLeftClick = () => {
-        setCurrentIndex((prev) => (prev === 0 ? images.length - 1 : prev - 1))
-    }
+    // const handleRightClick = () => {
+    //     setCurrentIndex((prev) => prev === images.length - 1 ? 0 : prev + 1)
+    // }
 
-    const handleRightClick = () => {
-        setCurrentIndex((prev) => prev === images.length - 1 ? 0 : prev + 1)
-    }
+    // useEffect(() => {
+    //     const interval = setInterval(() => {
+    //         setCurrentIndex((prev) => (prev === images.length - 1 ? 0 : prev + 1));
+    //     }, 5000);
+
+    //     return () => clearInterval(interval); // clean up on unmount
+    // }, [images.length]);
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setCurrentIndex((prev) => (prev === images.length - 1 ? 0 : prev + 1));
-        }, 5000);
+            setFadeClass('fade-out'); // trigger fade out
 
-        return () => clearInterval(interval); // clean up on unmount
+            setTimeout(() => {
+                setCurrentIndex((prev) => (prev + 1) % images.length);
+                // setIndex((prev) => (prev + 1) % groomImages.length)
+                setFadeClass('fade-image'); // trigger fade in
+            }, 2000); // match this to fadeOut duration
+        }, 7000); // how often image changes
+
+        return () => clearInterval(interval);
     }, [images.length]);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setGroomFadeClass('fade-out');
+    
+            setTimeout(() => {
+                setIndex((prev) => (prev + 1) % groomImages.length);
+                setGroomFadeClass('fade-image');
+            }, 2000); // fade out duration
+        }, 7000); // change interval
+    
+        return () => clearInterval(interval);
+    }, [groomImages.length]);
+
 
     return (
         <div className='apparels-containers'>
@@ -54,7 +86,7 @@ const Mensapparels = () => {
                         src={images[currentIndex]}
                         alt=""
                         width="450px"
-                        className="fade-image"
+                        className={fadeClass}
                     />
                     {/* <button className="arrow-button right" onClick={handleRightClick}>
                         <img src={right} alt="right" />
@@ -70,11 +102,12 @@ const Mensapparels = () => {
                     </button> */}
                     {/* <img src={images[currentIndex]} alt="" width="450px" /> */}
                     <img
-                        key={currentIndex}
-                        src={images[currentIndex]}
+                        key={index}
+                        src={groomImages[index]}
                         alt=""
                         width="450px"
-                        className="fade-image"
+                        height={"600px"}
+                        className={groomFadeClass}
                     />
                     {/* <button className="arrow-button right" onClick={handleRightClick}>
                         <img src={right} alt="right" />
